dd893d5391bdbda27a101477eb67d50aad622927
Vidura Priyadarshana <vidura200427@gmail.com>
2024-10-25 19:39:59 +0530

    complete admin menu bar

--- src/main/java/edu/ijse/datadish/controller/AdminDashController.java
+++ src/main/java/edu/ijse/datadish/controller/AdminDashController.java
@@ -2,44 +2,40 @@ package edu.ijse.datadish.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-
-import java.io.IOException;
 
 public class AdminDashController {
 
     @FXML
-    private Button btnHome;
+    private Button btnAddItem,btnResavtion,btnEmployee,btnHome,btnInventory,btnLogOut,btnReports;
 
     @FXML
-    private Button btnLogOut;
+    private AnchorPane loadPageAnchor,mainAnchor,menuAnchor;
 
     @FXML
-    private Button btnReports;
-
-    @FXML
-    private Button btnResavtion;
-
-    @FXML
-    private AnchorPane loadPageAnchor;
-
-    @FXML
-    private AnchorPane mainAnchor;
-
-    @FXML
-    private AnchorPane menuAnchor;
-
-    @FXML
-    void navigateToHomePage(ActionEvent event){
+    void navigateToAddItemPage(ActionEvent event) {
 
     }
 
     @FXML
-    void navigateToLogInPage(ActionEvent event) throws IOException {
-        mainAnchor.getChildren().clear();
-        mainAnchor.getChildren().add(FXMLLoader.load(getClass().getResource("/view/LogIn.fxml")));
+    void navigateToEmployeePage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void navigateToHomePage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void navigateToInventoryPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void navigateToLogInPage(ActionEvent event) {
+
     }
 
     @FXML
